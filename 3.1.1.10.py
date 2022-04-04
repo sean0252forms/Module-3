7401f4e0a83d21723c9765064cd5d8e23823dd06
Sean <75319944+sean0252forms@users.noreply.github.com>
2022-04-04 14:18:50 +0000

    Added stuff from desktop

---
+++ 3.1.1.10.py
@@ -0,0 +1,10 @@
+def plant():
+  plant=input('Type in a plant? ')
+  if(plant=='Spathiphyllum'):
+    print("Yes - Spathiphyllum is the best plant ever!")
+  else:
+    if(plant == 'spathiphyllum'):
+     print("No, I want a big Spathiphyllum!")
+    else:
+      print("Spathiphyllum! Not ",plant,"!")
+plant()
---
+++ 3.1.1.4.py
@@ -0,0 +1,6 @@
+n=int(input())
+print(n >=100)
+n=int(input())
+print(n >=100)
+n=int(input())
+print(n >=100)
