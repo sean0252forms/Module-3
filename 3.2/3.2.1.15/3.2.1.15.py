c310cc0fd511bd5db19085d07fd3e3cd23efba82
Sean <75319944+sean0252forms@users.noreply.github.com>
2022-04-07 13:22:42 +0000

    fixed 3.6.1.9

--- 3.2/3.2.1.15/3.2.1.15.py
+++ 3.2/3.2.1.15/3.2.1.15.py
@@ -1,7 +1,34 @@
+print("3.2.1.15")
+# c0=input("Input a number: ")
+# steps=0
+# evenNumbers=['2','4','6','8','0']
+# splitTxt=c0.split()
+# print(splitTxt)
+# while int(c0)!=1:
+# 	for i in evenNumbers:
+# 		if splitTxt[-1]==evenNumbers[i]:
+# 			continue
+# 		else:
+# 			c0=3*int(c0)+1
+# print(splitTxt)
 c0=input("Input a number: ")
 steps=0
-splitTxt=c0.split()
-print(splitTxt)
-while c0!=1:
-	splitTxt=c0.split()
+evenNumbers=['2','4','6','8','0']
+splitTxt=list(c0)
+c0=splitTxt
+newString=''
+print(c0)
+for i in splitTxt:
+	newString+=splitTxt[int(i)]
+string2=''
+for i in splitTxt:
+	string2+=newString[int(i)]
+print(string2)
+while int(string2)!=1:
+	for i in evenNumbers:
+		print(i)
+	if int(newString[len(newString)-1])==evenNumbers[i]:
+			c0=c0 / 2
+	else:
+			c0=3*int(c0)+1
 print(splitTxt)
--- 3.6.1.9/3.6.1.9.py
+++ 3.6.1.9/3.6.1.9.py
@@ -1,6 +1,5 @@
-my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
-#
-# Write your code here.
-#
+my_list = [1, 2, 4, 4, 1, 4, 2, 6,2, 9]
+new_list=list(dict.fromkeys(my_list))
 print("The list with unique elements only:")
 print(my_list)
+print(new_list)
